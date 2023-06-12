16718f30fffa3fb5e21f178f876017a48df78aca
hanab11 <sxakln2020@163.com>
2023-06-13 04:57:17 +0800

    标签属性ref

---
+++ 20_src_标签属性ref/App.vue
@@ -0,0 +1,51 @@
+<!--
+  ref属性：
+  1.被用来给元素或子组件注册引用信息（替代id）
+  2.应用在html标签上获取真实DOM元素，应用在组件标签上获取组件实例对象（vc）
+  3.使用方式：
+    打标识：<h1 ref="xxx"></h1> 或 <App ref="xxx"/>
+    获取：this.$refs.xxx
+-->
+
+
+<template>
+  <div>
+    <h1 v-text="msg" ref="title" id="title"></h1>
+    <button ref="btn" @click="showDOM">点我输出上方DOM元素</button>
+    <School ref="sch" id="sch"></School>
+  </div>
+</template>
+
+<script>
+import School from "./components/School.vue";
+
+export default {
+  name: "App",
+  components: {
+    School,
+  },
+  data() {
+    return {
+      msg: "欢迎学习Vue",
+    };
+  },
+  methods: {
+    showDOM() {
+      //console.log(document.getElementById("title")); //ref和id功能类似 但vue中尽量少做原生操作
+      console.log(this.$refs.title); //获取真实DOM元素
+      console.log(this.$refs.btn); //获取真实DOM元素
+
+      //但是组件标签上的ref和id有明显区别
+      console.log(
+        "通过ref访问组件标签",
+        this.$refs.sch,
+        this.$refs.sch.address
+      ); //获取School组件实例对象（vc）
+      console.log("通过id访问组件标签", document.getElementById("sch"));
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 20_src_标签属性ref/components/School.vue
@@ -0,0 +1,28 @@
+<!-- 组件的结构——html -->
+<template>
+  <div class="school">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+    
+<!-- 组件交互相关——js（包括数据、方法） -->
+<script>
+//默认暴露，以便在App组件头子中import引入（暴露一共3种方式）
+export default /*省略了Vue.extend()*/ {
+  name: "School",
+  data() {
+    return {
+      name: "硅谷",
+      address: "北京",
+    };
+  },
+};
+</script>
+    
+<!-- 组件的样式——css（非单文件组件的弊病：样式不能跟着组件走） -->
+<style>
+.school {
+  background-color: gray;
+}
+</style>
