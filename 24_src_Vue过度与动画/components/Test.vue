5e18689dcb646a9ac09a03bc26f572895ef590a3
hanab11 <sxakln2020@163.com>
2023-07-25 23:58:14 +0800

    过度与动画

---
+++ 24_src_Vue过度与动画/components/Test.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <!-- 用transition标签包裹，显示：appear缺省为true -->
+    <transition name="hello" appear>
+      <h1 v-show="isShow">你好啊！</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: orange;
+}
+
+/* enter-active和leave-active是vue动画样式的类名，默认v-，可自己绑定名字 */
+.hello-enter-active {
+  animation: atguigu 0.5s linear; /* 匀速 */
+}
+
+.hello-leave-active {
+  animation: atguigu 0.5s linear reverse; /* 匀速，反转 */
+}
+
+/* 动画写法：关键帧，使用animation: name调用 */
+@keyframes atguigu {
+  from {
+    transform: translateX(-100%); /* 移动，X横轴 */
+  }
+  to {
+    transform: translateX(0px); /* 0px为页面最右侧 */
+  }
+}
+</style>
---
+++ 24_src_Vue过度与动画/components/Test2.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <!-- 有多个元素，用transition-group标签，必须要有key  -->
+    <transition-group name="hello" appear>
+      <h1 v-show="isShow" key="1">尚硅谷！</h1>
+      <h1 v-show="!isShow" key="2">你好啊！</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: orange;
+}
+
+/* 6个vue的关键样式，用transition标签 */
+.hello-enter-active,
+.hello-leave-active {
+  transition: 0.5s linear; /* transition过度 */
+}
+/* 进入的起点、离开的终点 */
+.hello-enter,
+.hello-leave-to {
+  transform: translateX(-100%);
+}
+/* 进入的终点、离开的起点 */
+.hello-enter-to,
+.hello-leave {
+  transform: translateX(0);
+}
+</style>
