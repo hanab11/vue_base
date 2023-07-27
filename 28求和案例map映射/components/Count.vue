312efe399cb679b9a5ce9e665aad207d83e52bb0
hanab11 <sxakln2020@163.com>
2023-07-28 03:17:02 +0800

    求和案例_mapAction/mapMutation（自动生成的方法需要传参数，缺省值为event；只有同名时才可用数组写法）

--- 28求和案例map映射/components/Count.vue
+++ 28求和案例map映射/components/Count.vue
@@ -9,15 +9,15 @@
       <option value="2">2</option>
       <option value="3">3</option>
     </select>
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
-    <button @click="incrementOdd">当前求和为奇数再加</button>
-    <button @click="incrementWait">等一等再加</button>
+    <button @click="increment(n)">+</button>
+    <button @click="decrement(n)">-</button>
+    <button @click="incrementOdd(n)">当前求和为奇数再加</button>
+    <button @click="incrementWait(n)">等一等再加</button>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters, mapActions, mapMutations } from 'vuex'
 export default {
   name: 'Count',
   data() {
@@ -26,7 +26,7 @@ export default {
     }
   },
   computed: {
-    //一般计算属性注意：函数形式是简写，完整写法键值对，值是带get、set函数的对象
+    //以下程序员自己写的计算属性：函数形式是简写，完整写法键值对，值是带get、set函数的对象
     /* sum: {
       get() {
         return this.$store.state.sum
@@ -37,31 +37,37 @@ export default {
       return this.$store.state.sum
     }, */
 
-    //借助mapState生成计算属性，从state里读数据（对象写法）
+    //借助mapState自动生成计算属性，从state里读数据（对象写法）
     //注意字符串sum是state里的数据，且 sum:'sum' 不存在省略写法
     ...mapState({ sum: 'sum', school: 'school', subject: 'subject' }), //三点对象展开运算符
 
-    //借助mapState生成计算属性，从state里读数据（数组写法）可一定程度省略
+    //借助mapState生成计算属性，从state里读数据（数组写法）
+    //只有计算属性和state里的属性同名时才可一定程度省略
     //...mapState(['sum', 'school', 'subject']),
 
-    //同上，借助mapGetters
+    //同上，借助mapGetters自动生成
     ...mapGetters(['bigSum']),
   },
   methods: {
-    increment() {
-      //非业务逻辑，直接调用$store.commit提交
+    //借助mapMutations、mapActions，注意自动生成的方法需要传参数，缺省值为event
+    ...mapMutations({ increment: 'JIA', decrement: 'JIAN' }),
+    ...mapActions({ incrementOdd: 'jiaOdd', incrementWait: 'jiaWait' }),
+
+    //以下是程序员自己写的方法
+    //非业务逻辑，直接调用$store.commit提交
+    /* increment() {
       this.$store.commit('JIA', this.n)
     },
     decrement() {
       this.$store.commit('JIAN', this.n)
-    },
-    incrementOdd() {
-      //业务逻辑，就需要调用$store.dispatch派遣Actions先完成
+    }, */
+    //业务逻辑，就需要调用$store.dispatch派遣Actions先完成
+    /* incrementOdd() {
       this.$store.dispatch('jiaOdd', this.n)
     },
     incrementWait() {
       this.$store.dispatch('jiaWait', this.n)
-    },
+    }, */
   },
   mounted() {
     //mapState 的返回结果是对象{sum:function(),...}，可参考控制台
