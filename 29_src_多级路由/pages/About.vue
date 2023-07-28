732e6c1ca166630158c932d3cc8eac4e746a6411
hanab11 <sxakln2020@163.com>
2023-07-29 02:02:05 +0800

    多级路由

---
+++ 29_src_多级路由/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>我是About的内容</h2>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  mounted() {
+    console.log('About组件挂载完毕')
+    window.aRoute = this.$route
+    window.aRouter = this.$router
+  },
+  beforeDestroy() {
+    console.log('About组件即将销毁')
+  },
+}
+</script>
