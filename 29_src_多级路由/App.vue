e0deae7d1f54a5276bd33a2fa82b40828525aecd
hanab11 <sxakln2020@163.com>
2023-07-29 13:29:39 +0800

    多级路由_注释

--- 29_src_多级路由/App.vue
+++ 29_src_多级路由/App.vue
@@ -39,6 +39,7 @@
 
 <script>
 /*
+  0.使用路由用<router-link></router-link>，指定路由组件显示用<router-view></router-view>
   1.app只需要注册一般组件；About、Home这种路由组件当做页面等待配置，放在pages里
   2.没启动的路由组件默认会销毁掉，需要才挂载
   3.每个组件都有自己的$route，存自己的路由信息；所有组件共用一个$router
