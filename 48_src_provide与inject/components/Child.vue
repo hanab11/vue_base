940004e49e96b06efe5a5b7dab05b94196942c17
hanab11 <sxakln2020@163.com>
2023-09-05 19:08:30 +0800

    provide、inject，实现祖孙组件间通信（也可后代组件通信）

---
+++ 48_src_provide与inject/components/Child.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="child">
+		<h3>我是Child组件（子）</h3>
+		<Son />
+	</div>
+</template>
+
+<script>
+import { inject } from 'vue'
+import Son from './Son.vue'
+export default {
+	name: 'Child',
+	components: { Son },
+	// 也可后代组件通信
+	setup() {
+		let x = inject('car')
+		console.log(x, 'Child-----')
+	}
+}
+</script>
+
+<style>
+.child {
+	background-color: skyblue;
+	padding: 10px;
+}
+</style>
