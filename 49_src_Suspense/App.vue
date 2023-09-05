9f681dbd8543bd8daf0b07c7e6be0460dd9ed4cf
hanab11 <sxakln2020@163.com>
2023-09-05 19:13:04 +0800

    Suspense组件，等待异步的时候，渲染备用内容

---
+++ 49_src_Suspense/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="app">
+		<h3>我是App组件</h3>
+		<!-- 用Suspense把异步组件包起来，用插槽实现 -->
+		<Suspense>
+			<!-- 默认内容放default插槽 -->
+			<template v-slot:default>
+				<Child />
+			</template>
+			<!-- 备用内容放fallback插槽 -->
+			<template v-slot:fallback>
+				<h3>稍等，加载中...</h3>
+			</template>
+		</Suspense>
+	</div>
+</template>
+
+<script>
+// import Child from './components/Child.vue'// 静态引入
+import { defineAsyncComponent } from 'vue' // API（函数），定义异步组件
+const Child = defineAsyncComponent(() => import('./components/Child.vue')) // 异步引入
+export default {
+	name: 'App',
+	components: { Child },
+}
+</script>
+
+<style>
+.app {
+	background-color: gray;
+	padding: 10px;
+}
+</style>
---
+++ 49_src_Suspense/components/Child.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="child">
+		<h3>我是Child组件</h3>
+		{{ sum }}
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+	name: 'Child',
+	/**
+	 * 首先在App把Child定义异步组件，异步引入
+	 * setup就可以加async
+	 */
+	async setup() {
+		let sum = ref(0)
+		let p = new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve({ sum }) // 成功了就会把 {sum} 返回出去
+			}, 3000)
+		})
+		return await p
+	}
+}
+</script>
+
+<style>
+.child {
+	background-color: skyblue;
+	padding: 10px;
+}
+</style>
